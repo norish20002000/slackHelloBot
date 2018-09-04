13e70cb8d9c84666bc9b4284bcffb6242cd05c5f
shuro <norish20002000@gmail.com>
2018-09-04 17:58:20 +0900

    first commit app

---
+++ helloBot.py
@@ -0,0 +1,32 @@
+#!/usr/local/pyenv/shims/python
+# coding:utf-8
+
+import time
+import re
+from slackclient import SlackClient
+import AppConf
+
+class SlackBotMain:
+    sc = SlackClient(AppConf.token)
+
+    def __init__(self):
+        if SlackBotMain.sc.rtm_connect():
+            while True:
+                data = SlackBotMain.sc.rtm_read()
+
+                if len(data) > 0:
+                    for item in data:
+                        SlackBotMain.sc.rtm_send_message("shudo-memo", self.create_message(item))
+
+                    time.sleep(1)
+        else:
+            print("Connection Failed, invalid token?")
+
+    def create_message(self, data):
+        if "type" in data.keys():
+            if data["type"] == "message":
+                if re.search(u"(.*帰ります.*|.*帰宅.*)", data["text"]) is not None:
+                    return "<@" + data["user"] + ">" + u"お疲れ様。気をつけて帰ってください。:wink:"
+
+sbm = SlackBotMain()
+            
