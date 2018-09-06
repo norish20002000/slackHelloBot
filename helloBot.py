df5fca5231aed4a90e071d8ee51dc9ff4da9d041
shuro <norish20002000@gmail.com>
2018-09-06 13:10:38 +0900

    memcache追加

--- helloBot.py
+++ helloBot.py
@@ -29,25 +29,28 @@ class SlackBotMain:
         if re.search(u"(.*帰ります.*|.*帰宅.*)", data["text"]) is not None:
             return "<@" + data["user"] + ">" + u"お疲れ様。気をつけて帰ってください。:wink:"
         
-        if re.search(u"(.*戻ります.*|.*帰社.*)", data["text"]) is not None:
+        if re.search(u"(.*もどります.*|.*戻ります.*|.*帰社.*)", data["text"]) is not None:
             return "<@" + data["user"] + ">" + u"ご連絡ありがとうございます。気をつけて戻ってください。:wink:"
         
-        if re.search(u".*dbmain.*", data["text"]) is not None:
+        if re.search(u".*dbmain.*", data["text"], re.IGNORECASE) is not None:
             return "<@" + data["user"] + ">main系DBですね。:wink:" + "\n" + "```" + Constant.dbMainStr + "```\n\n"\
                     + "こちらで、よろしいですか。:relieved:"
 
-        if re.search(u".*dbres.*", data["text"]) is not None:
+        if re.search(u".*dbres.*", data["text"], re.IGNORECASE) is not None:
             return "<@" + data["user"] + ">res系DBですね。:wink:" + "\n" + "```" + Constant.dbResStr + "```\n\n"\
                     + "こちらで、よろしいですか。:relieved:"
 
-        if re.search(u".*dbmroo.*", data["text"]) is not None:
+        if re.search(u".*dbmroo.*", data["text"], re.IGNORECASE) is not None:
             return "<@" + data["user"] + ">search系DBですね。:wink:" + "\n" + "```" + Constant.dbMroongaStr + "```\n\n"\
                     + "こちらで、よろしいですか。:relieved:"
         
-        if re.search(u".*dbcass.*", data["text"]) is not None:
+        if re.search(u".*dbcass.*", data["text"], re.IGNORECASE) is not None:
             return "<@" + data["user"] + ">NoSqlのcassandraですね。:wink:" + "\n" + "```" + Constant.dbCassandraStr + "```\n\n"\
                     + "こちらで、よろしいですか。:relieved:"
 
+        if re.search(u".*dbmem.*", data["text"], re.IGNORECASE) is not None:
+            return "<@" + data["user"] + ">NoSqlのmemcacheですね。:wink:" + "\n" + "```" + Constant.dbMemcacheStr + "```\n\n"\
+                    + "こちらで、よろしいですか。:relieved:"
 
 sbm = SlackBotMain()
             
